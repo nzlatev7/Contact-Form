81920e206cafc477419f543c3c4d9803d84c197f
Nikolay <nikolay.zlatev2005@gmail.com>
2023-05-08 22:48:45 +0300

    adding db form that will be our interface of database

---
+++ Contact_Form/Database.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Database.aspx.cs" Inherits="Contact_Form.Database" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="gv" runat="server" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:BoundField DataField="Email" HeaderText="Email"/>
+                    <asp:BoundField DataField="Subject" HeaderText="Subject"/>
+                    <asp:BoundField DataField="FirstName" HeaderText="First Name"/>
+                    <asp:BoundField DataField="LastName" HeaderText="Last Name"/>
+                    <asp:BoundField DataField="Message" HeaderText="Message"/>
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Contact_Form/Database.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Contact_Form
+{
+    public partial class Database : System.Web.UI.Page
+    {
+        //connectionString
+        public string conString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog = Contact; Integrated Security = True";
+        //public string conString = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //зареждаме информацията от базата данни
+            SqlConnection sqlCon = new SqlConnection(conString);
+
+
+            sqlCon.Open();
+            SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM Information", sqlCon);
+            DataTable dtbl = new DataTable();
+            sqlDa.Fill(dtbl);
+
+            // bind our info with GridView 
+            gv.DataSource = dtbl;
+            gv.DataBind();
+
+        }
+    }
+}
